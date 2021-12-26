df134474ef46128f69bb1797fe71d1662cc7c64e
ArcherMatrix <sheldon.86@outlook.com>
2021-12-26 13:56:04 -0500

    js math practice

--- projects/Javascript/scripts/basic-math/sandbox.js
+++ projects/Javascript/scripts/basic-math/sandbox.js
@@ -1,18 +1,25 @@
 let x = 10;
 let y = 5;
+let z = 6;
 
-let val = x + y;
-console.log(`${x} + ${y} is ${val}`);
+let val = x + y + z;
+let val2;
+console.log(`${x} + ${y} + ${z} is ${val}`);
 
-val = x - y;
-console.log(`${x} - ${y} is ${val}`);
+val = x - y - z;
+console.log(`${x} - ${y} - ${z} is ${val}`);
 
-val = x * y;
-console.log(`${x} * ${y} is ${val}`);
+val = x * y * z;
+console.log(`${x} * ${y} * ${z} is ${val}`);
 
-val = x / y;
-console.log(`${x} / ${y} is ${val}`);
+val = x / y / z;
+console.log(`${x} / ${y} / ${z} is ${val}`);
 
 y = 3;
 val = x % y;
+val2 = x % z;
 console.log(`${x} % ${y} is ${val}`);
+console.log(`${x} % ${z} is ${val2}`);
+
+y=5;
+console.log(x *(y - z) );
