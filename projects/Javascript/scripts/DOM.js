154fa9ed2ac63a69aae4d7f09906d5555bc74890
ArcherMatrix <sheldon.86@outlook.com>
2021-12-28 14:39:48 -0500

    added DOMs lesson

--- projects/Javascript/scripts/DOM.js
+++ projects/Javascript/scripts/DOM.js
@@ -4,3 +4,6 @@ let newNode = document.createElement("p");
 newNode.innerHTML = "DOM manipulation is <strong>super cool</strong>!";
 
 div.appendChild(newNode);
+
+let p2 = div.getElementsByTagName("p")[1];
+div.removeChild(p2);
