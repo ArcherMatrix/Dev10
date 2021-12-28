f132890fdad62d52be76c843a083e3e7c2517fdd
ArcherMatrix <sheldon.86@outlook.com>
2021-12-28 15:35:40 -0500

    updated json and objects file

--- projects/Javascript/scripts/jsonObjects.js
+++ projects/Javascript/scripts/jsonObjects.js
@@ -1,3 +1,5 @@
+//Free-hand syntax
+
 let human = new Object;
 human.name = "Sheldon";
 human.age = 22;
@@ -7,3 +9,41 @@ human.sayHello = function()
 };
 
 human.sayHello();
+
+//Object Intialization Syntax
+
+let human2 =
+{
+    name:"Bobby",
+    age: 21,
+    sayHello: function()
+    {
+        console.log(`${this.name} says 'Wassup!'`);
+    }
+};
+
+human2.sayHello();
+
+//Associative Arrays
+
+console.log(human.age);
+
+//Comparing Objects
+
+console.log(typeof(human)==typeof(human2));
+
+console.log(human==human2);
+
+console.log(human);
+
+human2.sayBye = function()
+{
+    console.log(`${this.name} says 'Bye!'`);
+}
+
+console.log(typeof(human)==typeof(human2));
+
+//JSON
+let json_Sheldon = '{"name":"Sheldon","age":22}';
+let human3 = JSON.parse(json_Sheldon);
+console.log(human3);
