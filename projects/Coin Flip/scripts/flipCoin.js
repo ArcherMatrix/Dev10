3038ecf4dfc43dd4d5b6ab98c9c6fbdb3ee968d8
ArcherMatrix <sheldon.86@outlook.com>
2021-12-29 15:46:18 -0500

    finished coin flip function

--- projects/Coin Flip/scripts/flipCoin.js
+++ projects/Coin Flip/scripts/flipCoin.js
@@ -1,5 +1,38 @@
 function flipCoin(id)
 {
     let choice = document.getElementById(id);
-    console.log(choice.innerText);
+    //console.log(choice.innerText);
+
+    let result = Math.floor(Math.random()*2);
+    let pick;
+    switch(result)
+    {
+        case 0:
+            pick = "Heads";
+            //console.log(`CPU chose:${result}`);
+            break;
+        case 1:
+            pick = "Tails";
+            //console.log(`CPU chose:${result}`);
+            break;
+        default:
+            console.error("invalid number generation");
+            break;
+    }
+    if(choice.innerText == pick)
+    {
+        //console.log("correct");
+        let resultP = document.querySelector(".results");
+        let newNode =document.createElement("p");
+        newNode.innerHTML = "You are correct!";
+        resultP.appendChild(newNode);
+    }
+    else
+    {
+        //console.log("incorrect");
+        let resultP = document.querySelector(".results");
+        let newNode =document.createElement("p");
+        newNode.innerHTML = "You are incorrect!";
+        resultP.appendChild(newNode);
+    }
 };
