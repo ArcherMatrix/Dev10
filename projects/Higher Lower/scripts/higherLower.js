9e2167d6a53c2aed10ad9fa9c548259b4a02efca
ArcherMatrix <sheldon.86@outlook.com>
2022-01-01 14:51:51 -0500

    added the result paragraph and moved "Guess"

--- projects/Higher Lower/scripts/higherLower.js
+++ projects/Higher Lower/scripts/higherLower.js
@@ -8,14 +8,31 @@ function checkValue()
 
     if(input==rand)
     {
-        console.log("You got it!");
+        //console.log("You got it!");
+        resultParagraph("You got it!");
     }
     else if(input<rand)
     {
-        console.log("No, try a higher number.");
+        //console.log("No, try a higher number.");
+        resultParagraph("No, try a higher number.");
     }
     else if(input>rand)
     {
-        console.log("No, try a lower number.");
+        //console.log("No, try a lower number.");
+        resultParagraph("No, try a lower number.");
     }
+}
+function resultParagraph(result)
+{
+    let resultP = document.querySelector(".results");
+    let oldP = document.getElementById("resultP");
+    //console.log(oldP);
+    if(oldP!=null)
+    {
+        resultP.removeChild(oldP);
+    }
+    let newNode =document.createElement("p");
+    newNode.innerHTML = result;
+    newNode.id = "resultP";
+    resultP.appendChild(newNode);
 }
