028aeac69a9e9286cf3590256ecad73706f0c3af
ArcherMatrix <sheldon.86@outlook.com>
2022-01-01 15:16:38 -0500

    added colors to the results

--- projects/Higher Lower/scripts/higherLower.js
+++ projects/Higher Lower/scripts/higherLower.js
@@ -9,20 +9,20 @@ function checkValue()
     if(input==rand)
     {
         //console.log("You got it!");
-        resultParagraph("You got it!");
+        resultParagraph("You got it!",1);
     }
     else if(input<rand)
     {
         //console.log("No, try a higher number.");
-        resultParagraph("No, try a higher number.");
+        resultParagraph("No, try a higher number.",2);
     }
     else if(input>rand)
     {
         //console.log("No, try a lower number.");
-        resultParagraph("No, try a lower number.");
+        resultParagraph("No, try a lower number.",3);
     }
 }
-function resultParagraph(result)
+function resultParagraph(result,flag)
 {
     let resultP = document.querySelector(".results");
     let oldP = document.getElementById("resultP");
@@ -33,6 +33,20 @@ function resultParagraph(result)
     }
     let newNode =document.createElement("p");
     newNode.innerHTML = result;
+    newNode.style.fontWeight = "900";
     newNode.id = "resultP";
+    
+    switch(flag)
+    {
+        case 1:
+            newNode.style.color = "green";
+            break;
+        case 2:
+            newNode.style.color = "blue";
+            break;
+        case 3:
+            newNode.style.color = "red";
+
+    }
     resultP.appendChild(newNode);
 }
