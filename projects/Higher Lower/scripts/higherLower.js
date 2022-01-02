f9e336f6a3b8ebb2d04feb710639acc71378f93d
ArcherMatrix <sheldon.86@outlook.com>
2022-01-01 20:35:03 -0500

    fixed "else if" condition resolving incorrectly

--- projects/Higher Lower/scripts/higherLower.js
+++ projects/Higher Lower/scripts/higherLower.js
@@ -1,4 +1,5 @@
 let guesses = [];
+//Prompts user to enter the highest range
 let max = prompt("Enter the highest value");
 if(isNaN(max)||max===""||max === null)
 {
@@ -7,53 +8,65 @@ if(isNaN(max)||max===""||max === null)
 }
 else
 {
+    //Appends the upper range to the paragraph
     let instruction = document.querySelector(".instruction");
     let newP = document.createElement("p");
     newP.innerHTML = `Guess a number between 1 and ${max}.`; 
     instruction.appendChild(newP);
 }
 
-
-let rand = Math.floor(Math.random()*max)+1;
+//Generates the random number between 1 and the submitted range
+let rand = Math.floor(Math.random()*Number(max))+1;
 
 console.log(rand);
-
+//This function is called when the user clicks the "Guess" button.
 function checkValue()
 {
+    //Gets the user's guess from the textbox
     let input = document.getElementById('guessBox').value;
 
+    //Checks the entry to see if it is a number, empty string or null value
     if(isNaN(input)||input===""||input === null)
     {
         alert("Entered value is not a valid number.");
     }
-    else if(input<0||input>max)
+    //Checks to see if the entry is in between the 1 and upper range
+    else if(input < 1||input > Number(max))
     {
         alert("Entered value is not in range, try again.");
+        console.log(`Your Guess: ${input}`);
+        console.log(`The max: ${max}`);
     }
-    else if(input==rand)
+    //Checks to see if the entry has already been submitted
+    else if(guesses.includes(input))
     {
-        //console.log("You got it!");
-        guesses.push(input);
-        resultParagraph(`You got it! It took you ${guesses.length} tries and your guesses were ${guesses}.`,1);
-    }
-    else if(input<rand)
-    {
-        //console.log("No, try a higher number.");
-        guesses.push(input);
-        resultParagraph("No, try a higher number.",2);
-    }
-    else if(input>rand)
-    {
-        //console.log("No, try a lower number.");
-        guesses.push(input);
-        resultParagraph("No, try a lower number.",3);
+        alert("Entered value has already been entered. Choose a different value.");
     }
     else
     {
-        console.log("Error");
+        guesses.push(input);
+        if(input==rand)
+        {
+            //console.log("You got it!");
+            resultParagraph(`You got it! It took you ${guesses.length} tries and your guesses were ${guesses}.`,1);
+        }
+        else if(input<rand)
+        {
+            //console.log("No, try a higher number.");
+            resultParagraph("No, try a higher number.",2);
+        }
+        else if(input>rand)
+        {
+            //console.log("No, try a lower number.");
+            resultParagraph("No, try a lower number.",3);
+        }
+        else
+        {
+            console.log("Error");
+        }
     }
 }
-
+//This function adds the paragraph to the index.html with various colors depending on the guess result.
 function resultParagraph(result,flag)
 {
     let resultP = document.querySelector(".results");
@@ -78,7 +91,9 @@ function resultParagraph(result,flag)
             break;
         case 3:
             newNode.style.color = "red";
-
+            break;
+        default:
+            break;
     }
     resultP.appendChild(newNode);
 }
---
+++ projects/Javascript/scripts/test.js
@@ -0,0 +1,9 @@
+let input= 1;
+let max = 10;
+
+if(input<1||input>max)
+{
+    alert("Entered value is not in range, try again.");
+    console.log(`Your Guess: ${input}`);
+    console.log(`The max: ${max}`);
+}
